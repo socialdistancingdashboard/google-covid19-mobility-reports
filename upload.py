bcfd42858fbd145083327da6a4bf3e8fcaa8ef0c
Walther <thomas.walther01@sap.com>
2020-04-06 23:31:38 +0200

    buildspec

--- upload.py
+++ upload.py
@@ -5,4 +5,4 @@ import boto3
 client = boto3.client('s3')
 
 # Upload data to S3
-s3.Bucket('sdd-s3-bucket').upload_file('/data/processed/mobility_reports.csv','googlereports/'+str(date.year).zfill(4)+'/'+str(date.month).zfill(2)+'/'+str(date.day).zfill(2)')
+s3.Bucket('sdd-s3-bucket').upload_file('/data/processed/mobility_reports.csv',googlereports/'+str(date.year).zfill(4)+'/'+str(date.month).zfill(2)+'/'+str(date.day).zfill(2))
