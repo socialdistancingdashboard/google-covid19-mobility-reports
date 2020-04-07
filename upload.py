8612877c07237c2a80dcaa3628c91d9868f8f4df
Thomas Walther <tho.walther@gmail.com>
2020-04-08 00:29:38 +0200

    update source

--- upload.py
+++ upload.py
@@ -4,4 +4,4 @@ import boto3
 
 s3 = boto3.resource('s3')
 
-s3.meta.client.upload_file('/data/processed/mobility_reports.csv', "ssd-s3-bucket", "mobility_reports.csv")
+s3.meta.client.upload_file('data/processed/mobility_reports.csv', "ssd-s3-bucket", "mobility_reports.csv")
