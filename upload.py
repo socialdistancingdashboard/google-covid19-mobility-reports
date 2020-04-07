aaa2cbb7ee370289caf8e224723228cba56de6e7
Thomas Walther <tho.walther@gmail.com>
2020-04-08 00:17:10 +0200

    boto3

--- upload.py
+++ upload.py
@@ -2,8 +2,6 @@ import json
 from datetime import datetime, timedelta
 import boto3
 
-client = boto3.client('s3')
+s3 = boto3.resource('s3')
 
 s3.meta.client.upload_file('/data/processed/mobility_reports.csv', "ssd-s3-bucket", "mobility_reports.csv")
-
-
