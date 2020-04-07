2795326bde1978c4e458ed542f426ef814c48b56
Thomas Walther <tho.walther@gmail.com>
2020-04-08 00:59:10 +0200

    fix

--- upload.py
+++ upload.py
@@ -3,4 +3,5 @@ from datetime import datetime, timedelta
 import boto3
 
 s3 = boto3.resource('s3')
+date = datetime.now()
 s3.meta.client.upload_file('data/processed/mobility_reports.csv', "sdd-s3-bucket", 'mobility_reports/{}/{}/{}.csv'.format(str(date.year).zfill(4), str(date.month).zfill(2), str(date.day).zfill(2)))
